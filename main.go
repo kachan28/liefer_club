5ce23b897afd95de115f33213604e1ba82dfe098
kachan28 <kochkinma@mail.ru>
2022-07-31 14:49:49 +0300

    feat: working creating files

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "github.com/kachan28/liefer_club/cmd"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	protokolsFolder    = "LC-Kasse-Programmierprotokolle"
+	protokolMenuFolder = "JSON-Format"
+)
+
+var (
+	protokolPath = filepath.Join(protokolsFolder, protokolMenuFolder)
+)
 
 func main() {
-	cmd.Execute()
+	currPath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	if _, err := os.Stat(filepath.Join(currPath, protokolPath)); os.IsNotExist(err) {
+		err := os.MkdirAll(filepath.Join(currPath, protokolPath), 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+	jsonResult, err := json.Marshal(struct {
+		name    string `json:"name"`
+		surname string `json:"surname"`
+	}{
+		name:    "sad",
+		surname: "asd",
+	})
+	if err != nil {
+		panic(err)
+	}
+	protocolFileCreationTime := time.Now()
+	protocolFileName := fmt.Sprintf("%s_%s", "create_attempt", protocolFileCreationTime.Format("2006.01.02_15-04-05"))
+	err = os.WriteFile(filepath.Join(currPath, protokolPath, protocolFileName)+".json", jsonResult, 0644)
+	// err = os.WriteFile("temp.json", jsonResult, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
